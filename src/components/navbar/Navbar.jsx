79e50a5ca15bb25705d68caef1ae44441aab3787
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-10 19:26:26 +0530

    1st commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <div className="flex-column d-flex justify-content-between">
+          <img
+            src="https://www.clker.com/cliparts/s/b/G/J/5/4/monkey-cartoon-face-md.png"
+            alt="Something went wrong!!"
+            className="mx-2"
+            height="80"
+            width="80"
+          />
+          <div className="text-dark">Monkey News</div>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Link
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                href="/"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Categories
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" to="/business">
+                    Business
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/health">
+                    Health
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/science">
+                    Science
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/sports">
+                    Sports
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/technology">
+                    Technology
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
